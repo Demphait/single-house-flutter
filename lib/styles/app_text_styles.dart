c63c41f58395502467262db19f597f001b1fe927
Bogdan <bodya13371337@gmail.com>
2022-04-28 16:25:24 +0300

    feat: added new img feat: set brightness feat: added colors feat: added text styles

--- lib/styles/app_text_styles.dart
+++ lib/styles/app_text_styles.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:single_house/styles/app_colors.dart';
 
 extension AppTextStyles on TextStyle {
@@ -14,7 +14,20 @@ extension AppTextStyles on TextStyle {
     fontFamily: '',
   );
 
+  static TextStyle get styleTextTime => const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: '',
+  );
+
+  static TextStyle get styleSearchField => const TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w400,
+    fontFamily: '',
+  );
+
   TextStyle get black => copyWith(color: AppColors.black);
   TextStyle get white => copyWith(color: AppColors.white);
   TextStyle get primary => copyWith(color: AppColors.primary);
+  TextStyle get search => copyWith(color: AppColors.search);
 }
