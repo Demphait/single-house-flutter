b5fc897a22840d11c06101318f64df136218c2da
Bogdan <bodya13371337@gmail.com>
2022-05-11 20:46:26 +0300

    fix: improved AppTextField for validation added new color and textstyle for text error

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static Color get black => Colors.black;
   static Color get white => Colors.white;
+  static Color get red => Colors.red;
   static Color get primary => const Color(0xff5458F7);
   static Color get background => const Color(0xFF191A1D);
   static Color get search => const Color.fromARGB(150, 255, 255, 255);
