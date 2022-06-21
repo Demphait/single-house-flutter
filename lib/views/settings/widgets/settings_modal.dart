548b96c8902eaec28f77f25416fd7dc1d64f4308
Bogdan <bodya13371337@gmail.com>
2022-06-21 13:59:22 +0300

    feat: added a new universal widget for SettingWidget

---
+++ lib/views/settings/widgets/settings_modal.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:single_house/app/global_context.dart';
+import 'package:single_house/app/router/router_core.dart';
+import 'package:single_house/styles/app_button_styles.dart';
+import 'package:single_house/styles/app_space.dart';
+
+typedef SettingsModalCallBack = Future<bool> Function();
+
+class SettingsModal extends StatelessWidget {
+  SettingsModal({
+    Key? key,
+    required this.title,
+    required this.child,
+    this.onTap,
+  }) : super(key: key);
+  final String title;
+  final Widget child;
+  final SettingsModalCallBack? onTap;
+
+  final formKey = GlobalKey<FormState>();
+  List<Widget> _childWrapButton(BuildContext context) => [
+        Form(
+          key: formKey,
+          child: child,
+        ),
+        SizedBox(height: AppSpace.def),
+        ElevatedButton(
+          style: AppButtonStyles.primaryButton,
+          child: const Text('Save Changes'),
+          onPressed: () async {
+            //ECore.handle(code: 6, message: ''); //TODO
+            bool resultValidation = formKey.currentState?.validate() ?? false;
+            if (resultValidation) {
+              bool result = await onTap!.call();
+              if (result) {
+                RouterCore.pop(context: context);
+                GlobalContext.showSnackText('Changes saved');
+              }
+            }
+          },
+        ),
+      ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        AppSpace.def,
+        AppSpace.md,
+        AppSpace.def,
+        MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            title,
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          SizedBox(height: AppSpace.md),
+          if (onTap != null) ..._childWrapButton(context) else child,
+          SizedBox(height: AppSpace.def),
+        ],
+      ),
+    );
+  }
+}
